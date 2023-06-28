25d66d7a936b493a953d247ccc3fa6bdcffa45d3
Wu Liwei <wulw1990@gmail.com>
2023-06-28 20:12:59 +0800

    Initial Commit

---
+++ SQLite3Framework/SQLite3Framework.h
@@ -0,0 +1,18 @@
+//
+//  SQLite3Framework.h
+//  SQLite3Framework
+//
+//  Created by MacBook Air on 2023/6/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SQLite3Framework.
+FOUNDATION_EXPORT double SQLite3FrameworkVersionNumber;
+
+//! Project version string for SQLite3Framework.
+FOUNDATION_EXPORT const unsigned char SQLite3FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLite3Framework/PublicHeader.h>
+
+
